03ac4d3e3765ac2e7066d69d8500af33675a2391
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-03-15 17:35:55 +0530

    Add APB_slave2 module for APB communication

---
+++ APB/slave2.v
@@ -0,0 +1,36 @@
+module APB_slave2(
+         input PCLK,
+         input PRSTn,  
+         input PSEL2,
+         input PENABLE,
+         input PWRITE,
+         input [8:0]PADDR,     
+         input [7:0]PWDATA,
+  
+         output reg [7:0]PRDATA,
+         output reg PREADY
+);
+
+      reg [31:0] mem [0:255];
+
+  always @(posedge PCLK or negedge PRSTn) begin
+    if (!PRSTn) begin
+      PRDATA  <= 32'b0;
+      PREADY  <= 1'b0;
+    end
+    else if (PSEL2 && PENABLE) begin
+      if (PWRITE) begin
+        mem[PADDR] <= PWDATA;  
+        PREADY     <= 1'b1;
+      end
+      else begin
+        PRDATA  <= mem[PADDR];
+        PREADY  <= 1'b1;
+      end
+    end
+    else begin
+      PREADY  <= 1'b0;
+    end
+  end
+
+endmodule
